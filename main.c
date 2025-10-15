652d5fb1c3a0f7554e71489027b05e21b519c35f
jw2511281 <jw2511281@sookmyung.ac.kr>
2025-10-15 22:48:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func(void) {
-     int x;
-     printf("func x is at %p\n",&x);
+int sumTwo(int a, int b)
+{
+    return (a+b);
 }
 
-int main(void) {
-    int x;
-    printf("main x is at %p\n",&x);
-    func();
+int square(int n)
+{
+    return (n*n);
+}
+
+int get_max(int x, int y)
+{
+    if (x>y)
+       return x;
+    else
+        return y;
+}
+
+int main(int argc, char *argv[]) {
     
+    printf("sum two result is %i\n", sumTwo(2,3));
+    printf("square result is %i\n", square(5));
+    printf("get max result is %i\n", get_max(10,9));
+    
+    system("PAUSE");
     return 0;
 }
+    
