c80df99c8f7a1f5c7a66fbed262ad216af111190
jw2511281 <jw2511281@sookmyung.ac.kr>
2025-10-15 22:53:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
     
     printf("sum two result is %i\n", sumTwo(2,3));
     printf("square result is %i\n", square(5));
-    printf("get max result is %i\n", get_max(10,9));
+    printf("get max result is %i\n", get_max(10,13));
     
     system("PAUSE");
     return 0;
